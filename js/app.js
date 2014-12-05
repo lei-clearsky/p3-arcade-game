3142390371a80ff0c9c64efbf4b22ef2d96e7511
lei-clearsky <sportzhulei@gmail.com>
2014-12-05 15:11:21 -0500

    add JSDoc comments

--- js/app.js
+++ js/app.js
@@ -1,19 +1,49 @@
+/**
+ * @fileoverview Classes for the arcade game logic
+ * @author sportzhulei@gmail.com (Lei Zhu)
+ */
 
-var Game = function(){
-    // minimal enemy speed
+ /**
+  * Game class
+  * The Game class construct, initialize the game and handles logics of the game
+  * @constructor
+  */
+
+var Game = function() {
+    /**
+     * Enemy's minimum speed
+     * @type {number}
+     */
     this.minEnemySpeed = 100;
-    // maximen enemy speed
+    /**
+     * Enemy's maximum speed
+     * @type {number}
+     */
     this.maxEnemySpeed = 300;
-    // score
+    /**
+     * Record the game's score
+     * @type {number}
+     */
     this.score = 0;
-    // life
+    /**
+     * Record the game's lives
+     * @type {number}
+     */
     this.life = 3;
+    /**
+     * Set the game to stop or continue
+     * @type {boolean}
+     */
     // set stop to false when initialize the game, 
     // when player lost all lives or no time, set stop to true to stop the game
     this.stop = false;
-    // initialize enemy array
+    /**
+     * Enimies in the game
+     * Once game starts each enimy will be initialized and added to this array
+     * @type {Array.<Enemy>}
+     */
     this.allEnemies = [];
-    // add enemy objects to the enemy array
+    // Initializes enimies and add them to the {@code this.allEnemies}
     this.initEnemies();
     // initialize a new player
     this.player = new Player();
@@ -32,184 +62,257 @@ var Game = function(){
 
         that.player.handleInput(allowedKeys[e.keyCode]);
     });
-}
-
-// initialize enemy objects and put them into the allEnemies array
-Game.prototype.initEnemies = function(){
+};
+/**
+  * Initialize enemy objects and put them into the allEnemies array
+  * @return {void}
+  */
+Game.prototype.initEnemies = function() {
     // initialize four enemies on each row
     // loop through four enemy rows
-    for(var i = 0; i < 4; i++){
+    for (var i = 0; i < 4; i++) {
         var enemy = new Enemy();
         // return random numbers between minEnemySpeed and maxEnemySpeed
         enemy.speed = Math.floor(Math.random()*this.maxEnemySpeed + this.minEnemySpeed); 
         // push each enemy to allEnemies array
         this.allEnemies.push(enemy);
     }
-}
-
-// Check collisions
-Game.prototype.checkCollisions = function(){
-    for (var i = 0; i < this.allEnemies.length; i++){
-        if (Math.abs(this.player.x - this.allEnemies[i].x) < 50 && Math.abs(this.player.y - this.allEnemies[i].y) < 50){
+};
+/**
+  * Check if there are collisions between the player and enemies
+  * @return {void}
+  */
+Game.prototype.checkCollisions = function() {
+    for (var i = 0; i < this.allEnemies.length; i++) {
+        if (Math.abs(this.player.x - this.allEnemies[i].x) < 50 && Math.abs(this.player.y - this.allEnemies[i].y) < 50) {
             // if player is hit, reset player position
             this.player.reset();
-            if (this.life > 0){
+            if (this.life > 0) {
                 // if player's life is more than 0, subtract one life
                 this.life --;
                 // update life
-                document.getElementById('life').innerHTML = "Life: "+this.life;
+                document.getElementById('life').innerHTML = 'Life: ' + this.life;
             }
         }
     }
-} 
-
-// Change stats or enemies behavior after player collect items, and update stats
-Game.prototype.checkPlayerHelpers = function(){
+};
+/**
+  * Change stats or enemies behavior after player collect items, and update stats
+  * @return {void}
+  */
+Game.prototype.checkPlayerHelpers = function() {
     // if the player collect an item
-    if (Math.abs(this.player.x - this.playerHelper.x) < 50 && Math.abs(this.player.y - this.playerHelper.y) < 50){
+    if (Math.abs(this.player.x - this.playerHelper.x) < 50 && Math.abs(this.player.y - this.playerHelper.y) < 50) {
         // if the player collects a heart, add one life
-        if(this.playerHelper.sprite == 'images/Heart.png'){
+        if (this.playerHelper.sprite == 'images/Heart.png') {
             this.life ++;
-            document.getElementById('life').innerHTML = "Life: "+this.life;
+            document.getElementById('life').innerHTML = 'Life: ' + this.life;
         // if the player collects a blue gem, slow enemies speed for one second
-        }else if(this.playerHelper.sprite == 'images/Gem Blue.png'){
+        } else if (this.playerHelper.sprite == 'images/Gem Blue.png') {
             // save enemies original speed
             var originalEnemySpeeds = new Array(3);
             var allEnemies = this.allEnemies;
             // slow each enemy's speed
-            for(var i = 0; i < allEnemies.length; i++){
+            for (var i = 0; i < allEnemies.length; i++) {
                 originalEnemySpeeds[i] = allEnemies[i].speed;
                 allEnemies[i].speed = allEnemies[i].speed / 3;
             }     
             // change back to original speed after one second
-            setTimeout(function () {
-                for(var i = 0; i < originalEnemySpeeds.length; i++){
+            setTimeout(function() {
+                for (var i = 0; i < originalEnemySpeeds.length; i++) {
                     allEnemies[i].speed = originalEnemySpeeds[i];
                 } 
             }, 1000); 
         // if the player collects a green gem, add two points
-        }else if(this.playerHelper.sprite == 'images/Gem Green.png'){
+        } else if (this.playerHelper.sprite == 'images/Gem Green.png') {
             this.score += 2;
-            document.getElementById('score').innerHTML = "Score: "+this.score;
+            document.getElementById('score').innerHTML = 'Score: ' + this.score;
         // if the player collects a orange gem, add five points
-        }else if(this.playerHelper.sprite == 'images/Gem Orange.png'){
+        } else if (this.playerHelper.sprite == 'images/Gem Orange.png') {
             this.score += 5;
-            document.getElementById('score').innerHTML = "Score: "+this.score;
+            document.getElementById('score').innerHTML = 'Score: ' + this.score;
         // if the player collects a rock, it will lose one life :( don't collect rocks in this game..
-        }else if(this.playerHelper.sprite == 'images/Rock.png'){
+        } else if (this.playerHelper.sprite == 'images/Rock.png') {
             this.life --;
-            document.getElementById('life').innerHTML = "Life: "+this.life;
-        }
+            document.getElementById('life').innerHTML = 'Life: ' + this.life;
+        };
         // once the player hit the helpers, move the helper off the screen;
         this.playerHelper.x = -100;
         this.playerHelper.y = -100;
-    }
+    };
 
-} 
-
-// If player hit water, reset the player location, player will lose one life.
-// Otherwise, the player reaches the destination, increase score
-Game.prototype.checkDestination = function(){
-    if(this.player.y < 0) {
-        if((this.player.x > 10 && this.player.x < 200) || (this.player.x > 200 && this.player.x < 400)) {
+}; 
+/**
+  * Check if player hit water or reach destination
+  * If player hit water, reset the player location, player will lose one life
+  * If player reach destination, increase score
+  * @return {void}
+  */
+Game.prototype.checkDestination = function() {
+    if (this.player.y < 0) {
+        if ((this.player.x > 10 && this.player.x < 200) || (this.player.x > 200 && this.player.x < 400)) {
             this.player.reset();
             this.score ++;
-            document.getElementById('score').innerHTML = "Score: "+this.score;
-        }else {
+            document.getElementById('score').innerHTML = 'Score: ' + this.score;
+        } else {
             this.player.reset();
             this.life --;
-            document.getElementById('life').innerHTML = "Life: "+this.life;
-        }
-    }
-}
-// check if the game need stop, if life is zero, set stop to true and game over.
-Game.prototype.render = function(){
+            document.getElementById('life').innerHTML = 'Life: ' + this.life;
+        };
+    };
+};
+/**
+  * Check if the game needs to be stopped 
+  * If total life is zero, the game will stop
+  * @return {void}
+  */
+Game.prototype.render = function() {
     if(this.life == 0){
         this.stop = true;
         this.gameOver();
-    }
-}
-// game over message
-Game.prototype.gameOver = function(){
+    };
+};
+/**
+  * Set the gameOver message
+  * @return {void}
+  */
+Game.prototype.gameOver = function() {
     var gameBoard = document.getElementById('game-board');
     gameBoard.parentNode.removeChild(gameBoard);
-    var gameOverMessage = document.getElementById('game-over-message');
-    var zeroLifeMessage = "Look like your life is zero :( "+ "<br>";
-    var gameScoreMessage = "Your final score is " + game.score;
+    var gameOverMessage = document.getElementById('gameOver-message');
+    var zeroLifeMessage = 'Look like your life is zero :( ' + '<br>';
+    var gameScoreMessage = 'Your final score is ' + game.score;
     // if life is zero, display zero life message; if run out of time, display the score
-    if(this.life === 0){
+    if (this.life === 0){
         gameOverMessage.innerHTML = zeroLifeMessage + gameScoreMessage;
-    }else{
+    } else {
         gameOverMessage.innerHTML = gameScoreMessage;
-    }    
-}
-
-// Enemy object
+    };    
+};
+/**
+  * Enemy class.
+  * Constructs an enemy in the game
+  * @constructor
+  */
 var Enemy = function() {
-    // enemy's y values
+    /**
+     * Enemy's all y position values
+     * @type {Array.<number>}
+     */
 	this.enemyY = [60,145,230,315];
-    // enemy's starting x value
+    /**
+     * Enemy's x position values
+     * @type {number}
+     */
     this.x = -101;
-    // randomnize enemy's y values 
+    /**
+     * Enemy's randomnized y position values
+     * @type {number}
+     */
 	this.y = this.enemyY[Math.round(Math.random()*3)];
+
 	this.speed;
     // load enemy's image
     this.sprite = 'images/enemy-bug.png';
-}
-
-// Update the enemy's position
-// Parameter: dt, a time delta between ticks
+};
+/**
+  * Update the enemy's position
+  * @param dt A time delta between ticks
+  */
 Enemy.prototype.update = function(dt) {
     // move enemy
     this.x += this.speed * dt;
     // If our enemies move off the screen, restart them at one block (101px) 
     // right before the start of the screen.  
-    if(this.x > 500) {
+    if (this.x > 500) {
         this.x = -101;
         // randomnize enemy's y value every time enemy move off 
         // the screen and start from the begining again
         this.y = this.enemyY[Math.round(Math.random()*3)];
-    }
+    };
     
-}
-
-// Draw the enemy on the screen
+};
+/**
+ * Renders and draws the enemy on the screen
+ * @return {void}
+ */ 
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
-// Items player can collect
+};
+/**
+  * Item class.
+  * Constructs item that player can collect during the game
+  * @constructor
+  */
 var Item = function(){
+    /**
+     * Item's all x position values
+     * @type {Array.<number>}
+     */
     this.itemX = [0,100,200,300,400];
+    /**
+     * Item's all y position values
+     * @type {Array.<number>}
+     */
     this.itemY = [80,160,240,320];
+    /**
+     * Item's x position value
+     * @type {number}
+     */
     this.x = this.startPosX();
+    /**
+     * Item's y position value
+     * @type {number}
+     */
     this.y = this.startPosY();
-}
-
-Item.prototype.startPosX = function () {
+};
+/**
+ * Set the x position value of the item
+ * @return {number}
+ */ 
+Item.prototype.startPosX = function() {
     var startX = this.itemX[Math.round(Math.random()*this.itemX.length)];
     return startX;
-}
-
+};
+/**
+ * Set the y position value of the item
+ * @return {number}
+ */
 Item.prototype.startPosY = function() {
     var startY = this.itemY[Math.round(Math.random()*this.itemY.length)];
     return startY;
-}
-
+};
+/**
+ * Update item's position
+ * @param {number} dt A time delta between ticks
+ * @return {void}
+ */
 Item.prototype.update = function(dt) {
     this.x*dt;
     this.y*dt;
-}
-
+};
+/**
+ * Reset item's position
+ * @return {void}
+ */
 Item.prototype.reset = function() {
     this.x = this.startPosX();
     this.y = this.startPosY();
-}
-
+};
+/**
+ * Render and draws an item in the game
+ * @return {void}
+ */
 Item.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
-// PlayerHelper inherites Item 
-var PlayerHelper = function () {
+};
+/**
+  * PlayerHelper class.
+  * Constructs an helper item that player can collect during the game
+  * @constructor
+  * @extends {Item}
+  */
+var PlayerHelper = function() {
     Item.call(this);
     this.loadNewHelper();
     this.reset();
@@ -218,70 +321,93 @@ var PlayerHelper = function () {
 PlayerHelper.prototype = Object.create(Item.prototype);
 // set PlayerHelper constructor
 PlayerHelper.prototype.constructor = PlayerHelper;
-// load one new helper
-PlayerHelper.prototype.loadNewHelper = function(){
+/**
+ * Loads an random new helper item
+ * @return {void}
+ */
+PlayerHelper.prototype.loadNewHelper = function() {
     this.spriteOptions = ['images/Rock.png','images/Rock.png','images/Rock.png','images/Heart.png','images/Gem Blue.png', 'images/Gem Green.png', 'images/Gem Orange.png'];
     this.sprite = this.spriteOptions[Math.floor(Math.random()*this.spriteOptions.length)];
-}
-
-PlayerHelper.prototype.render = function () {
+};
+/**
+ * Renders and draws a new helper item
+ * @return {void}
+ */
+PlayerHelper.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-
-PlayerHelper.prototype.reset = function(){
+/**
+ * Reset helper item's position every five seconds
+ * @return {void}
+ */
+PlayerHelper.prototype.reset = function() {
     var that = this;
     // move the helper off the screen;
     that.x = -100;
     that.y = -100;
-    setInterval(function(){
+    setInterval(function() {
         that.loadNewHelper();
         Item.prototype.reset.call(that);    
     }, 5000);
-}
-
-
-// Player constructor
+};
+/**
+  * Player class.
+  * Constructs a player
+  * @constructor
+  */
 var Player = function() {
-	this.sprite = 'images/char-boy.png';
+    this.sprite = 'images/char-boy.png';
     this.x = 200;
     this.y = 400;
-}
-
+};
+/**
+ * Update player's position
+ * @param {number} dt A time delta between ticks
+ * @return {void}
+ */
 Player.prototype.update = function(dt) {
     this.x*dt;
     this.y*dt;
-}
-
+};
+/**
+ * Reset player's position
+ * @return {void}
+ */
 Player.prototype.reset = function() {
     this.x = 200;
     this.y = 400;
-}
-
+};
+/**
+ * Renders and draws a player in the game
+ * @return {void}
+ */
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
-// player can use keyboard to move in the game
-Player.prototype.handleInput = function(key){
+};
+/**
+ * Handles keyboard pressed events
+ * @param {string} key The id of pressed key
+ * @return {void}
+ */
+Player.prototype.handleInput = function(key) {
 	switch(key) {
         case 'left':
-            if(this.x > 0)
+            if (this.x > 0)
             this.x -= 100;
             break;
         case 'up':
-            if(this.y > 0)
+            if (this.y > 0)
             this.y -= 90;
             break;
         case 'right':
-            if(this.x < 400)
+            if (this.x < 400)
             this.x += 100;
             break;
         case 'down':
-            if(this.y < 375)
+            if (this.y < 375)
             this.y += 90;
             break;
         default:
             return;
-    }
-}
-
-
+    };
+};
--- js/engine.js
+++ js/engine.js
@@ -1,6 +1,7 @@
 // game engine
 var Engine = (function(global) {
-    /* Predefine the variables we'll be using within this scope,
+    /**
+     * Predefine the variables we'll be using within this scope,
      * create the canvas element, grab the 2D context for that canvas
      * set the canvas elements height/width and add it to the DOM.
      */
@@ -17,36 +18,46 @@ var Engine = (function(global) {
     canvas.height = 606;
     doc.getElementById('game-board').appendChild(canvas);
 
-    /* This function serves as the kickoff point for the game loop itself
-     * and handles properly calling the update and render methods.
+    /**
+     * Sets up the game loop itself and calls the update and render methods
+     * @return {void}
      */
     function main() {
-        /* Get our time delta information which is required if your game
+        /**
+         * Get our time delta information which is required if your game
          * requires smooth animation. Because everyone's computer processes
          * instructions at different speeds we need a constant value that
          * would be the same for everyone (regardless of how fast their
-         * computer is) - hurray time!
+         * computer is).
          */
         var now = Date.now(),
             dt = (now - lastTime) / 1000.0;
-        /* Call our update/render functions, pass along the time delta to
+        /**
+         * Call our update/render functions, pass along the time delta to
          * our update function since it may be used for smooth animation.
          */
         update(dt);
         render();
-        /* Set our lastTime variable which is used to determine the time delta
+        /**
+         * Set our lastTime variable which is used to determine the time delta
          * for the next time this function is called.
          */
         lastTime = now;
-        /* Use the browser's requestAnimationFrame function to call this
+        /**
+         * Use the browser's requestAnimationFrame function to call this
          * function again as soon as the browser is able to draw another frame.
          * if game's stop value set to true, stop the game.
          */
         if(!game.stop){
             win.requestAnimationFrame(main);
-        }
+        };
     };
-    // set a game timer 
+    /**
+     * Sets a timer for the game
+     * @param {number} time Total seconds of the timer
+     * @param {Game} game The new game instance
+     * @return {void}
+     */
     function setTimer(time, game) {
         timer = doc.getElementById('timer');
         seconds = time;
@@ -55,7 +66,7 @@ var Engine = (function(global) {
         if (seconds === 0) {
           game.gameOver();
           game.stop = true;
-        }
+        };
         // if game's stop value is false, update timer and continue the game
         if(!game.stop){
             seconds--;
@@ -63,30 +74,38 @@ var Engine = (function(global) {
             win.setTimeout(function(){
                 setTimer(seconds, game);
             }, 1000); 
-        }
-    } 
-    // update timer for each second
-    function updateTimer(){
+        };
+    }; 
+    /**
+     * Updates the timer each second
+     * @return {void}
+     */
+    function updateTimer() {
         var timerStr;
         var tempSeconds = seconds;
         var tempMinutes = Math.floor(seconds / 60) % 60;
         tempSeconds -= tempMinutes * 60;
-
         timerStr = formatTimer(tempMinutes, tempSeconds);
         timer.innerHTML = timerStr;
-
-    }
-    // format timer string that is displayed
-    function formatTimer(minutes, seconds){
-        var formattedMinutes = (minutes < 10) ? "0" + minutes : minutes;
-        var formattedSeconds = (seconds < 10) ? "0" + seconds : seconds;
+    };
+    /**
+     * Format timer string displayed in the game
+     * @param {number} minutes Remaining minutes of the timer
+     * @param {number} seconds Ramaining seconds of the timer
+     * @return {string}
+     */   
+    function formatTimer(minutes, seconds) {
+        var formattedMinutes = (minutes < 10) ? '0' + minutes : minutes;
+        var formattedSeconds = (seconds < 10) ? '0' + seconds : seconds;
 
         return formattedMinutes + ":" + formattedSeconds;
-    }
+    };
 
-    /* This function does some initial setup that should only occur once,
+    /**
+     * This function does some initial setup that should only occur once,
      * particularly setting the lastTime variable that is required for the
      * game loop.
+     * return {void}
      */
     function init() {
         reset();
@@ -95,13 +114,13 @@ var Engine = (function(global) {
            main();          
            setTimer(60, game);
 
-           doc.getElementById('score').innerHTML = "Score: "+game.score;
-           doc.getElementById('life').innerHTML = "Life: "+game.life;
-           doc.getElementById('timer').style.display = "inline-block";
-           doc.getElementById('life').style.display = "inline-block";
-           doc.getElementById('score').style.display = "inline-block";          
-           doc.getElementById('try-again').style.display = "inline-block";
-           doc.getElementById('game-board').style.display = "inline-block";
+           doc.getElementById('score').innerHTML = 'Score: ' + game.score;
+           doc.getElementById('life').innerHTML = 'Life: ' + game.life;
+           doc.getElementById('timer').style.display = 'inline-block';
+           doc.getElementById('life').style.display = 'inline-block';
+           doc.getElementById('score').style.display = 'inline-block';          
+           doc.getElementById('try-again').style.display = 'inline-block';
+           doc.getElementById('game-board').style.display = 'inline-block';
            
            var instruction = doc.getElementById('instruction');
            instruction.parentNode.removeChild(instruction);
@@ -109,22 +128,28 @@ var Engine = (function(global) {
            startButton.parentNode.removeChild(startButton);
            var heading = doc.getElementById('heading');
            heading.parentNode.removeChild(heading);
-       } 
-    }
+       }; 
+    };
 
-    /* This function is called by main (our game loop) and itself calls all
+    /** 
+     * This function is called by main (our game loop) and itself calls all
      * of the functions which may need to update entity's data. 
+     * @param {number} dt A time delta between ticks
+     * @return {void}
      */
     function update(dt) {
         updateEntities(dt);
         game.checkCollisions();
 		game.checkDestination();
         game.checkPlayerHelpers();
-    }
+    };
 
-    /* This is called by the update function  and loops through all of the
-     * objects within your allEnemies array as defined in app.js and calls
+    /**
+     * This is called by the update function and loops through all of the
+     * objects within the allEnemies array as defined in app.js and calls
      * their update() methods. 
+     * @param {number} dt A time delta between ticks
+     * @return {void}
      */
     function updateEntities(dt) {
         game.allEnemies.forEach(function(enemy) {
@@ -132,16 +157,19 @@ var Engine = (function(global) {
         });
         game.player.update(dt);
         game.playerHelper.update(dt);
-    }
+    };
 
-    /* This function initially draws the "game level", it will then call
+    /**
+     * This function initially draws the "game level", it will then call
      * the renderEntities function. Remember, this function is called every
      * game tick (or loop of the game engine) because that's how games work -
      * they are flipbooks creating the illusion of animation but in reality
      * they are just drawing the entire screen over and over.
+     * @return {void}
      */
     function render() {
-        /* This array holds the relative URL to the image used
+        /**
+         * This array holds the relative URL to the image used
          * for that particular row of the game level.
          */
 		var topRowImages = [
@@ -150,7 +178,7 @@ var Engine = (function(global) {
 			'images/water-block.png',   
             'images/stone-block.png',
 			'images/water-block.png'  
-		],
+		    ],
 			rowImages = [
                 'images/water-block.png',   // Top row is water
                 'images/stone-block.png',   // Row 1 of 3 of stone
@@ -163,42 +191,38 @@ var Engine = (function(global) {
             numCols = 5,
             row, col;
 
-        /* Loop through the number of rows and columns defined above
+        /**
+         * Loop through the number of rows and columns defined above
          * and, using the rowImages array, draw the correct image for that
          * portion of the "grid"
-        */
-		for (col = 0; col < numCols; col++){
+         */
+		for (col = 0; col < numCols; col++) {
 			ctx.drawImage(Resources.get(topRowImages[col]), col * 101, 0);
-		}
+		};
 		
         for (row = 1; row < numRows; row++) {
             for (col = 0; col < numCols; col++) {
                 ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
-            }
-        }
+            };
+        };
 
-            renderEntities();
-    }
+        renderEntities();
+    };
 
-    /* This function is called by the render function and is called on each game
+    /**
+     * This function is called by the render function and is called on each game
      * tick. It's purpose is to then call the render functions defined
      * on enemy and player entities within app.js
+     * return {void}
      */
     function renderEntities() {       
-            game.allEnemies.forEach(function(enemy) {
-                enemy.render();
-            });
-            game.player.render();            
-            game.playerHelper.render();           
-            game.render();          
-    }
-
-    /* This function does nothing but it could have been a good place to
-     * handle game reset states
-     */
-    function reset() {
-
-    }
+        game.allEnemies.forEach(function(enemy) {
+            enemy.render();
+        });
+        game.player.render();            
+        game.playerHelper.render();           
+        game.render();          
+    };
 
     // load all of the images
     Resources.load([
@@ -215,7 +239,8 @@ var Engine = (function(global) {
     ]);
     Resources.onReady(init);
 
-    /* Assign the canvas' context object to the global variable (the window
+    /**
+     * Assign the canvas' context object to the global variable (the window
      * object when run in a browser) so that it can be used more easily
      * from within their app.js files.
      */
